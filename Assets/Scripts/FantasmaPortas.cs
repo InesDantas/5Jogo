9df72ea3bf64b6aed1e86761bc70ad2a013e94a9
InesDantas <40200369@esmad.ipp.pt>
2021-05-23 11:33:12 +0100

    Initial commit

---
+++ Assets/Scripts/FantasmaPortas.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FantasmaPortas : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 5;
+
+    private Rigidbody fantasma;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fantasma = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translate = Input.GetAxis("Horizontal") * velocidade * Time.deltaTime;
+        transform.Translate(translate, 0, 0);
+
+        float updown = Input.GetAxis("Vertical") * velocidade * Time.deltaTime;
+        transform.Translate(0, 0, updown);
+    }
+}
