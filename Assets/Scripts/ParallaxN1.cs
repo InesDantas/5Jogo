ae6d8b17c6d114e307a32cc72e3d8fd338d1e3d2
aRN934 <61988380+aRN934@users.noreply.github.com>
2021-05-30 02:03:23 +0100

    Nivel01_PARALLAXBABYYY

---
+++ Assets/Scripts/ParallaxN1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxN1 : MonoBehaviour
+{
+
+    private float length, startposition;
+
+    public GameObject camera;
+
+    public float parallaxEffect;
+
+    void Start()
+    {
+        startposition = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+ 
+    void FixedUpdate()
+    {
+        float distance = (camera.transform.position.x * parallaxEffect);
+        transform.position = new Vector3(startposition + distance, transform.position.y, transform.position.z);
+    }
+}
