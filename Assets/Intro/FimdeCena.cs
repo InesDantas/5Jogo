dcb3581acd0c9638cc339d659bef15c87de3379e
Noctis112 <ruimartins96_@hotmail.com>
2021-06-07 20:35:44 +0100

    Intro completa

---
+++ Assets/Intro/FimdeCena.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FimdeCena : MonoBehaviour
+{
+ 
+   [SerializeField] GameObject Cena;
+    void OnEnable()
+    {
+        Cena.SetActive(false);
+    }
+    
+}
