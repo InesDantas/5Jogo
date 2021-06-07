f07040d795ac633d6948b2db0e774ad537c1a326
Noctis112 <ruimartins96_@hotmail.com>
2021-06-07 05:14:48 +0100

    Ambiente lvl1, Intro, Cinemachine, Visual Effect Graph

---
+++ Assets/Scripts/FogoRoxo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogoRoxo : MonoBehaviour
+{
+    [SerializeField]Light luzRoxa;
+    [SerializeField]float intensidadeMax = 20f;
+    [SerializeField]GameObject luz;
+    [SerializeField]float duracao = 2f;
+    void Update()
+    {
+        var amplitude = Mathf.PingPong(Time.time, duracao);
+        amplitude = amplitude / duracao * 0.5f + 0.5f;
+        luzRoxa.intensity = intensidadeMax * amplitude;
+    }
+}
