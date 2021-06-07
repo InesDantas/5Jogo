7f3bdbd4bb3614e973b638de00932f234bc185c0
Noctis112 <ruimartins96_@hotmail.com>
2021-06-08 00:18:36 +0100

    Scene Lv5

---
+++ Assets/Scripts/Base1Lv5.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base1Lv5 : MonoBehaviour
+{
+    [SerializeField] Material matBase;
+
+    float duracao = 1f;
+
+    void Start()
+    {
+        matBase.SetFloat("Vector1_54DC5DFC", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var amplitude = Mathf.PingPong(Time.time, duracao);
+        amplitude = amplitude / duracao * 0.1f + 0.1f;
+        matBase.SetFloat("Vector1_54DC5DFC", amplitude);
+    }
+}
