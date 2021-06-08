cf83bd7734df1721bc91d51d0d0c5661820d07ad
IagoFernandes1999 <72970480+IagoFernandes1999@users.noreply.github.com>
2021-06-08 05:45:47 +0100

    SOM DOS PASSOS E DO NIVEL 1

---
+++ Assets/Sons/WALKCYCLE/WalkCycleSom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkCycleSom : MonoBehaviour
+{
+    public AudioSource walkCycleSound;
+    void Update()
+
+    //adaptado de https://www.youtube.com/watch?v=DMJfQbgz_zo e https://answers.unity.com/questions/1502776/how-do-i-play-a-sound-when-pressing-the-w-key-and.html
+    {
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            if(!walkCycleSound.isPlaying)
+            {
+                walkCycleSound.Play();
+            }
+        }
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            walkCycleSound.Stop();
+        }
+       if (Input.GetButtonDown("Jump"))
+        {
+            walkCycleSound.Stop();
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            walkCycleSound.Play();
+        }
+
+    }
+}
+
