ebd8e5c0f20b53de435b456044fdcf3f0c9d5a37
IagoFernandes1999 <72970480+IagoFernandes1999@users.noreply.github.com>
2021-06-08 12:11:24 +0100

    UI FIM JOGO APARECER

---
+++ Assets/Menu/UiAparecer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiAparecer : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject menuFim;
+
+    void OnTriggerEnter()
+    {
+        menuFim.SetActive(true);
+    }
+
+    void Pause()
+    {
+        menuFim.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
