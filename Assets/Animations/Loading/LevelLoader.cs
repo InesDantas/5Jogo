61fa9ec8c891c1035866b1bb0693b90e941680fa
Noctis112 <ruimartins96_@hotmail.com>
2021-06-09 03:12:23 +0100

    Animação de fade in fade out

--- Assets/Animations/Loading/LevelLoader.cs
+++ Assets/Animations/Loading/LevelLoader.cs
@@ -1,37 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Gameplay : MonoBehaviour
+public class LevelLoader : MonoBehaviour
 {
     public Animator transition;
     public float transitionTime = 1f;
 
-
-    public GameObject coletavelOriginal;
-
-    private int pontos = 0;
-
-    [SerializeField] Text textoPontos;
-
     void Update()
     {
-        
     }
 
-    public void Pontuacao()
+    public void LoadNextLevel()
     {
-        pontos++;
-        textoPontos.text = pontos.ToString() + "/5";
 
-        if (pontos == 5)
-        {
-            Debug.Log("Yay");
-            StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
-        }
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
+
     IEnumerator LoadLevel(int levelIndex)
     {
         transition.SetTrigger("Start");
