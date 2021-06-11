03d8e59ff99ffdf3623dfd99e143713a6024aea3
IagoFernandes1999 <72970480+IagoFernandes1999@users.noreply.github.com>
2021-06-11 12:33:02 +0100

    PROGRAMACAO DOS PASSOS NO 3 NIVEL

---
+++ Assets/Sons/WALKCYCLE/WalkCycleNivel3.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkCycleNivel3 : MonoBehaviour
+{
+   
+    public AudioSource walkCycleSound;
+    void Update()
+
+    //adaptado de https://www.youtube.com/watch?v=DMJfQbgz_zo e https://answers.unity.com/questions/1502776/how-do-i-play-a-sound-when-pressing-the-w-key-and.html
+    {
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            if (!walkCycleSound.isPlaying)
+            {
+                walkCycleSound.Play();
+            }
+        }
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            walkCycleSound.Stop();
+        }
+        if (Input.GetButtonDown("Vertical"))
+        {
+            if (!walkCycleSound.isPlaying)
+            {
+                walkCycleSound.Play();
+            }
+        }
+        if (Input.GetButtonUp("Vertical"))
+        {
+            walkCycleSound.Stop();
+        }
+    }
+}
+
